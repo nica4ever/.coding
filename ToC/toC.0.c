d0c2e7a0216b1e1df058c159c957f40fea797df7
Nika <cojanuionut9@gmail.com>
2026-01-13 13:05:37 +0100

    Moved all ToC files in the ToC dir

---
+++ ToC/toC.0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* This is a comment example
+ *
+ *
+ */
+
+int main (void)
+{
+    printf ("To C or not to C\n"); // This is a comment example
+    printf ("That is the question\n");
+    return 0;
+    /****************************
+     * This is a comment example*
+     * *************************/
+}
